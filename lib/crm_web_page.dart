052be9ecd80229ab65677dac27cf574d4f23d55f
Divyansh Vishwakarma <ansh1111smith@gmail.com>
2024-04-02 21:35:26 +0530

    Initial

--- lib/crm_web_page.dart
+++ lib/crm_web_page.dart
@@ -1,70 +1,24 @@
 import 'package:almanet/hover_buttoons.dart';
+import 'package:almanet/responsive/desktop_body.dart';
+import 'package:almanet/responsive/mobile_body.dart';
+import 'package:almanet/responsive/responsive.dart';
 
 import 'package:flutter/material.dart';
 
-class CRMPage extends StatelessWidget {
+class CRMPage extends StatefulWidget {
   const CRMPage({super.key});
 
   @override
+  State<CRMPage> createState() => _CRMPageState();
+}
+
+class _CRMPageState extends State<CRMPage> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 8.0, top: 5),
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "CRM",
-            style: TextStyle(
-              fontSize: 35,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          actions: [
-            Hover_Button(
-              text: 'Sales',
-              defaultcolor: Colors.grey.shade600,
-              hovercolor: Colors.blue,
-              ontap: () {},
-              fontsize: 23,
-              width: 0,
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            Hover_Button(
-              text: 'Reporting',
-              defaultcolor: Colors.grey.shade600,
-              hovercolor: Colors.blue,
-              ontap: () {},
-              fontsize: 23,
-              width: 0,
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            Hover_Button(
-              text: 'Configuration',
-              defaultcolor: Colors.grey.shade600,
-              hovercolor: Colors.blue,
-              ontap: () {},
-              fontsize: 23,
-              width: 0,
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            InkWell(
-              child: const Icon(Icons.settings),
-              onTap: () {},
-            )
-          ],
-          leading: InkWell(
-              onTap: () {},
-              child: const Icon(
-                Icons.dehaze_rounded,
-                size: 35,
-              )),
-        ),
+    return const Scaffold(
+      body: Responsive(
+        mobile: MobileBody(),
+        desktop: DesktopBody(),
       ),
     );
   }
---
+++ lib/responsive/mobile_body.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import '../hover_buttoons.dart';
+
+class MobileBody extends StatelessWidget {
+  const MobileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0, ),
+      child: Scaffold(
+        appBar: AppBar(
+
+          title: const Text(
+            "CRM",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            Hover_Button(
+              text: 'Sales',
+              defaultcolor: Colors.grey.shade600,
+              hovercolor: Colors.blue,
+              ontap: () {},
+              fontsize: 23,
+              width: 0,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Hover_Button(
+              text: 'Reporting',
+              defaultcolor: Colors.grey.shade600,
+              hovercolor: Colors.blue,
+              ontap: () {},
+              fontsize: 23,
+              width: 0,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Hover_Button(
+              text: 'Configuration',
+              defaultcolor: Colors.grey.shade600,
+              hovercolor: Colors.blue,
+              ontap: () {},
+              fontsize: 23,
+              width: 0,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            InkWell(
+              child: const Icon(Icons.settings),
+              onTap: () {},
+            )
+          ],
+          leading: InkWell(
+              onTap: () {},
+              child: const Icon(
+                Icons.dehaze_rounded,
+                size: 32,
+              )),
+        ),
+      ),
+    );
+    ();
+  }
+}
